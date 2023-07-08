78ca680026ef1a0e0e92e7042a57de1a6563b067
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-07-08 17:56:07 -0400

    feat: implemented project prototype

--- script/DeploySwapper.s.sol
+++ script/DeploySwapper.s.sol
@@ -6,12 +6,14 @@ import {Swapper} from "src/Swapper.sol";
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "forge-std/console.sol";
 
+import {CometInterface} from "lib/comet/contracts/CometInterface.sol";
+
 contract DeploySwapperScript is Script {
     function setUp() public {}
 
     function run() public {
         uint256 priv_key = vm.envUint("PRIV_KEY");
-        address deployer_add = vm.addr(priv_key);
+        // address deployer_add = vm.addr(priv_key);
         address cUsdcAddress = vm.envAddress("cUSDC_ADDRESS");
         address usdcAddress = vm.envAddress("USDC_ADDRESS");
 
@@ -21,7 +23,8 @@ contract DeploySwapperScript is Script {
 
         console.log("SWAPPER INSTANCE IS", address(instance));
 
-        uint256 balance = IERC20(usdcAddress).balanceOf(deployer_add);
+        // uint256 balance = IERC20(usdcAddress).balanceOf(deployer_add);
+        uint256 balance = 10_000000;
 
         IERC20(usdcAddress).approve(address(instance), balance);
 
--- src/Swapper.sol
+++ src/Swapper.sol
@@ -2,65 +2,90 @@
 pragma solidity ^0.8.13;
 
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {CometInterface} from "lib/comet/contracts/CometInterface.sol";
 import "forge-std/console.sol";
-
-interface ICERC20 is IERC20 {
-    function supply(address asset, uint amount) external;
-
-    function redeemUnderlying(uint redeemAmount) external returns (uint);
-}
 
 contract Swapper {
     uint256 public number;
-    ICERC20 cUSDC;
+    CometInterface cUSDC;
     IERC20 uSDC;
 
     constructor(address _usdc, address _cUcdc) {
         uSDC = IERC20(_usdc);
-        cUSDC = ICERC20(_cUcdc);
+        cUSDC = CometInterface(_cUcdc);
     }
 
+    /// Allows `msg.sender` to deposit `amount` of tokens into the compound protocol and receive cTokens in exchange.
+    ///
+    /// @param amount The amount of tokens to be deposited into the compound protocol.
     function deposit(uint256 amount) external {
-        uint256 checkApproval = uSDC.allowance(msg.sender, address(this));
-        // uint256 initialContractBalance = uSDC.balanceOf(address(this));
+        // Checkpoint values
+        uint256 initialUsdcBalance = uSDC.balanceOf(address(this));
+        uint256 initialCUsdcBalance = cUSDC.balanceOf(address(this));
 
-        if (checkApproval < amount) {
+        uint256 approvedAmount = uSDC.allowance(msg.sender, address(this));
+
+        if (approvedAmount < amount) {
             revert("User did not approve enough allowance for this contract");
         }
 
-        console.log("USER DID APPROVE SWAPPER");
-
-        bool checkTransfer = uSDC.transferFrom(
-            msg.sender,
-            address(this),
-            amount
-        );
+        // Deposit `msg.sender` USDC into this contract account.
+        bool checkTransfer = uSDC.transferFrom(msg.sender, address(this), amount);
 
         if (!checkTransfer) {
             revert("USDC transfer from User account to Contract failed");
         }
 
-        console.log(
-            "RUGGED USER FROM ITS USDC BALANCE",
-            uSDC.balanceOf(address(this))
-        );
-
+        // Approve the cUSDC contract to transfer USDC to the compound protocol contract.
         bool check = uSDC.approve(address(cUSDC), amount);
 
         if (!check) {
             revert("Failed approval");
         }
 
+        // Deposit `amount` USDC into the compound protocol to receive cUSDC.
         cUSDC.supply(address(uSDC), amount);
 
-        // if (errorCode != 0) {
-        //     revert("COMPOUND ERROR WHILE MINTING");
-        // }
+        uint256 newCUsdcBalance = cUSDC.balanceOf(address(this));
 
-        checkTransfer = cUSDC.transfer(msg.sender, amount);
+        // Calculate how many cUSDC we received from the protocol.
+        uint256 dep = newCUsdcBalance - initialCUsdcBalance;
+
+        checkTransfer = cUSDC.transfer(msg.sender, dep);
 
         if (!checkTransfer) {
             revert("cUSDC transfer from Contract to User failed");
         }
+
+        // Final checks
+        uint256 finalUsdcBalance = uSDC.balanceOf(address(this));
+
+        if (finalUsdcBalance != initialUsdcBalance) {
+            revert("Final Usdc balance and initial usdc balance should be equal");
+        }
+
+        uint256 finalCUsdcBalance = cUSDC.balanceOf(address(this));
+
+        if (finalCUsdcBalance != initialCUsdcBalance) {
+            revert("Final cUSDC balance and initial USDC balance should be equal");
+        }
+    }
+
+    function withdraw(uint256 amount) external {
+        uint256 balance = cUSDC.balanceOf(address(this));
+
+        console.log("CONTRACT cUSDC BALANCE IS", balance, "USDC BALANCE IS", uSDC.balanceOf(address(this)));
+
+        cUSDC.withdraw(address(uSDC), amount);
+
+        console.log("SUCCESFULLY WITHDRWAN cUSDC", balance);
+
+        uint256 usdcBalance = uSDC.balanceOf(address(this));
+
+        console.log("CONTRACT USDC BALANCE IS", usdcBalance, "cUSDC BALANCE IS", cUSDC.balanceOf(address(this)));
+
+        bool check = uSDC.transfer(msg.sender, usdcBalance);
+
+        require(check, "USDC WITHDRAWAL TO USER FAILED");
     }
 }
