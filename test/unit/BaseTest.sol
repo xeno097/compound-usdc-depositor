34a28dc66db592cbd169253863bb5db88b56e3d0
Sebastiano Faiella <xeno097.cp@gmail.com>
2023-07-09 16:51:51 -0400

    refactor(test): implemented the BaseTest contract

---
+++ test/unit/BaseTest.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {CometMainInterface} from "lib/comet/contracts/CometInterface.sol";
+
+contract BaseTest is Test {
+    function mockERC20BalanceOfCall(address target, address account) internal {
+        mockERC20BalanceOfCall(target, account, 0);
+    }
+
+    function mockERC20BalanceOfCall(
+        address target,
+        address account,
+        uint256 ret
+    ) internal {
+        vm.mockCall(
+            target,
+            abi.encodeWithSelector(IERC20.balanceOf.selector, account),
+            abi.encode(ret)
+        );
+    }
+
+    function mockERC20AllowanceCall(
+        address target,
+        address account,
+        address targetAccount,
+        uint256 ret
+    ) internal {
+        vm.mockCall(
+            target,
+            abi.encodeWithSelector(
+                IERC20.allowance.selector,
+                account,
+                targetAccount
+            ),
+            abi.encode(ret)
+        );
+    }
+
+    function mockERC20AllowanceCall(
+        address target,
+        address account,
+        address targetAccount
+    ) internal {
+        mockERC20AllowanceCall(target, account, targetAccount, 0);
+    }
+
+    function mockERC20TransferFromCall(
+        address targetContract,
+        address from,
+        address to,
+        uint256 amount,
+        bool ret
+    ) internal {
+        vm.mockCall(
+            targetContract,
+            abi.encodeWithSelector(
+                IERC20.transferFrom.selector,
+                from,
+                to,
+                amount
+            ),
+            abi.encode(ret)
+        );
+    }
+
+    function mockERC20ApproveCall(
+        address targetContract,
+        address spender,
+        uint256 amount,
+        bool ret
+    ) internal {
+        vm.mockCall(
+            targetContract,
+            abi.encodeWithSelector(IERC20.approve.selector, spender, amount),
+            abi.encode(ret)
+        );
+    }
+
+    function mockCometTokenSupplyCall(
+        address targetContract,
+        address token,
+        uint256 amount
+    ) internal {
+        vm.mockCall(
+            targetContract,
+            abi.encodeWithSelector(
+                CometMainInterface.supply.selector,
+                token,
+                amount
+            ),
+            abi.encode()
+        );
+    }
+
+    function mockERC20TokenTransferCall(
+        address targetContract,
+        address to,
+        uint256 amount,
+        bool ret
+    ) internal {
+        vm.mockCall(
+            targetContract,
+            abi.encodeWithSelector(IERC20.transfer.selector, to, amount),
+            abi.encode(ret)
+        );
+    }
+}
--- test/unit/CompoundUsdcDepositor.t.sol
+++ test/unit/CompoundUsdcDepositor.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "./BaseTest.sol";
 import {CompoundUsdcDepositor} from "src/CompoundUsdcDepositor.sol";
 import {Errors} from "src/libs/Errors.sol";
 import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
@@ -9,7 +9,7 @@ import {CometMainInterface} from "lib/comet/contracts/CometInterface.sol";
 import {ERC20Mock} from "test/mocks/ERC20Mock.sol";
 import {CTokenMock} from "test/mocks/cTokenMock.sol";
 
-contract CompoundDepositorDepositTests is Test {
+contract CompoundDepositorDepositTests is BaseTest {
     CompoundUsdcDepositor instance;
     address usdcContractAddress;
     address compoundUsdcContractAddres;
@@ -28,109 +28,6 @@ contract CompoundDepositorDepositTests is Test {
         );
     }
 
-    function mockERC20BalanceOfCall(address target, address account) internal {
-        mockERC20BalanceOfCall(target, account, 0);
-    }
-
-    function mockERC20BalanceOfCall(
-        address target,
-        address account,
-        uint256 ret
-    ) internal {
-        vm.mockCall(
-            target,
-            abi.encodeWithSelector(IERC20.balanceOf.selector, account),
-            abi.encode(ret)
-        );
-    }
-
-    function mockERC20AllowanceCall(
-        address target,
-        address account,
-        address targetAccount,
-        uint256 ret
-    ) internal {
-        vm.mockCall(
-            target,
-            abi.encodeWithSelector(
-                IERC20.allowance.selector,
-                account,
-                targetAccount
-            ),
-            abi.encode(ret)
-        );
-    }
-
-    function mockERC20AllowanceCall(
-        address target,
-        address account,
-        address targetAccount
-    ) internal {
-        mockERC20AllowanceCall(target, account, targetAccount, 0);
-    }
-
-    function mockERC20TransferFromCall(
-        address targetContract,
-        address from,
-        address to,
-        uint256 amount,
-        bool ret
-    ) internal {
-        vm.mockCall(
-            targetContract,
-            abi.encodeWithSelector(
-                IERC20.transferFrom.selector,
-                from,
-                to,
-                amount
-            ),
-            abi.encode(ret)
-        );
-    }
-
-    function mockERC20ApproveCall(
-        address targetContract,
-        address spender,
-        uint256 amount,
-        bool ret
-    ) internal {
-        vm.mockCall(
-            targetContract,
-            abi.encodeWithSelector(IERC20.approve.selector, spender, amount),
-            abi.encode(ret)
-        );
-    }
-
-    function mockCometTokenSupplyCall(
-        address targetContract,
-        address token,
-        uint256 amount
-    ) internal {
-        vm.mockCall(
-            targetContract,
-            abi.encodeWithSelector(
-                CometMainInterface.supply.selector,
-                token,
-                amount
-            ),
-            abi.encode()
-        );
-    }
-
-    function mockERC20TokenTransferCall(
-        address targetContract,
-        address to,
-        uint256 amount,
-        bool ret
-    ) internal {
-        vm.mockCall(
-            targetContract,
-            abi.encodeWithSelector(IERC20.transfer.selector, to, amount),
-            abi.encode(ret)
-        );
-    }
-
-    // TESTS
     function testCannotDepositIfUserDidNotSetUsdcAllowance(
         uint256 amount
     ) external {
